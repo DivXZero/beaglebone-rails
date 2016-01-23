2884f02551525d6beb85f64a6baf1798c3887de6
DivXZero <silv3rlogic@gmail.com>
2016-01-22 19:10:21 -0800

    Removed failing test

--- lib/beaglebone/rails/version.rb
+++ lib/beaglebone/rails/version.rb
@@ -1,5 +1,5 @@
 module Beaglebone
   module Rails
-    VERSION = "0.1.0"
+    VERSION = "0.0.0"
   end
 end
--- spec/beaglebone/rails_spec.rb
+++ spec/beaglebone/rails_spec.rb
@@ -4,8 +4,4 @@ describe Beaglebone::Rails do
   it 'has a version number' do
     expect(Beaglebone::Rails::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
