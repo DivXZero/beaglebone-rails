f7436f78453f1eecfe73cabbb3aac0b650907b9d
DivXZero <silv3rlogic@gmail.com>
2016-01-23 20:28:10 -0800

    Fixed GPIO.list parsing

--- lib/beaglebone/GPIO.rb
+++ lib/beaglebone/GPIO.rb
@@ -4,7 +4,10 @@ module Beaglebone
     def self.list
       str = File.open('/sys/class/gpio')
       str.gsub! "\n", ','
-      str.gsub! '\n', ','
+
+      if !str.split(',').include?('export')
+        str.gsub! '\n', ','
+
       str.split(',')
     end
   end
