7f6acdd923fee9de46b9c0414fb0d0cc42b89b42
DivXZero <silv3rlogic@gmail.com>
2016-01-23 20:39:08 -0800

    Fixed GPIO.list parsing

--- lib/beaglebone/GPIO.rb
+++ lib/beaglebone/GPIO.rb
@@ -3,15 +3,9 @@ module Beaglebone
   module GPIO
     def self.list
       gpio_array = Dir.entries('/sys/class/gpio')
-      #gpio_array.remove('.')
-      #gpio_array.remove('..')
-
-      #str = File.open('/sys/class/gpio').read
-      #str.gsub! "\n", ','
-
-      #str.gsub! '\n', ',' unless str.split(',').include?('export')
-
-      #str.split(',')
+      gpio_array.delete('.')
+      gpio_array.delete('..')
+      gpio_array
     end
   end
 end
