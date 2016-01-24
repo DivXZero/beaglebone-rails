f77c6979eced395ae297f73394a7bd39ea8a7635
DivXZero <silv3rlogic@gmail.com>
2016-01-23 20:50:06 -0800

    Cleaned up GPIO.list

--- lib/beaglebone/GPIO.rb
+++ lib/beaglebone/GPIO.rb
@@ -1,10 +1,16 @@
 
 module Beaglebone
   module GPIO
-    def self.list
+    def self.list(list_all = false)
       gpio_array = Dir.entries('/sys/class/gpio')
       gpio_array.delete('.')
       gpio_array.delete('..')
+
+      if !list_all?
+        gpio_array.delete('export')
+        gpio_array.delete('unexport')
+      end
+
       gpio_array
     end
   end
