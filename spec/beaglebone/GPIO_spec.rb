de07fe8afe15884eb5eaa34a262581cc1f8933f9
DivXZero <silv3rlogic@gmail.com>
2016-01-23 20:54:15 -0800

    Cleaned up specs

--- spec/beaglebone/GPIO_spec.rb
+++ spec/beaglebone/GPIO_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Beaglebone::GPIO do
-  it 'list should contain export, unexport' do
+  it 'list contains export, unexport' do
     expect(Beaglebone::GPIO.list(true)).to include('export', 'unexport')
   end
 end
