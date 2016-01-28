527d5c0b5b5e77cc4ef351fdd3c0158c46a1451c
DivXZero <silv3rlogic@gmail.com>
2016-01-27 21:20:21 -0800

    Add gpio pin enable

--- lib/beaglebone-rails.rb
+++ lib/beaglebone-rails.rb
@@ -3,6 +3,8 @@ require 'beaglebone/GPIO/GPIO'
 require 'beaglebone/GPIO/pins'
 require 'beaglebone/version'
 
+# Top Level Module
+#
 module Beaglebone
 
   # Just a friendly greeting :)
--- lib/beaglebone/GPIO/GPIO.rb
+++ lib/beaglebone/GPIO/GPIO.rb
@@ -1,13 +1,17 @@
 
 module Beaglebone
+  # General Purpose I/O
+  #
   module GPIO
 
     # Returns an array of active GPIO pins
     #
-    # @param [Bool] optional List all active pins including export and unexport utilites
+    # @param list_all [Bool] optional List all active pins including export and unexport utilites
     #
     # @example Return all active GPIO pins
     #   Beaglebone::GPIO.list #=> ["gpiochip32", "gpiochip64", "gpiochip96", "gpiochip0"]
+    #
+    # @return [Array] String array of active gpio pins
     #
     def self.list(list_all = false)
       gpio_array = Dir.entries('/sys/class/gpio')
@@ -20,6 +24,21 @@ module Beaglebone
       end
 
       gpio_array
+    end
+
+    # Enables a pin for GPIO
+    #
+    # @param pin [Object] Specifies a value from GPIO::PINS to enable
+    #
+    # @example
+    #   Beaglebone::GPIO.enable_pin(:P9_12)
+    #
+    def self.enable_pin(pin)
+      pin = PINS[pin]
+      export = File.open('/sys/class/gpio/export', 'w')
+      return false if pin.nil? or export.nil?
+      export.puts(pin)
+      return true
     end
 
   end
