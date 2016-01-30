b17df12ee058353fc117202ca745662e675ac185
DivXZero <silv3rlogic@gmail.com>
2016-01-30 11:27:16 -0800

    Add install script

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 chown root:root 99-beaglebone-rails.rules
 chmod 0644 99-beaglebone-rails.rules
 cp 99-beaglebone-rails.rules /etc/udev/rules.d/
+udevadm control --reload-rules
+udevadm test --action=add /class/gpio
