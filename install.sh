baabd97975094ad8c0dd028ce356ab286a0b0b73
DivXZero <silv3rlogic@gmail.com>
2016-01-30 20:16:15 -0800

    Modify install script and udev rules

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-chown root:root 99-beaglebone-rails.rules
-chmod 0644 99-beaglebone-rails.rules
 cp 99-beaglebone-rails.rules /etc/udev/rules.d/
+chown root:root /etc/udev/rules.d/99-beaglebone-rails.rules
+chmod 0644 /etc/udev/rules.d/99-beaglebone-rails.rules
 udevadm control --reload-rules
-udevadm test --action=add /class/gpio
+#udevadm test --action=add /class/gpio
