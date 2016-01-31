770bf5a81dcb995f1aba403fdced3f70a6dbc2d3
DivXZero <silv3rlogic@gmail.com>
2016-01-30 20:19:30 -0800

    Modify install script to include test

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp 99-beaglebone-rails.rules /etc/udev/rules.d/
 chown root:root /etc/udev/rules.d/99-beaglebone-rails.rules
 chmod 0644 /etc/udev/rules.d/99-beaglebone-rails.rules
 udevadm control --reload-rules
-#udevadm test --action=add /class/gpio
+udevadm test --action=add /class/gpio
